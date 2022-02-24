823787fa9097582bd15b6fd3ba9a0c1a3687c91a
hsnysf <hasan.lmra@gmail.com>
2022-02-24 11:29:51 +0300

    Delete jaxrs/learn directory

--- jaxrs/learn/application/CompanyApplication.java
+++
@@ -1,9 +0,0 @@
-package jaxrs.learn.application;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class CompanyApplication extends Application{
-
-}
